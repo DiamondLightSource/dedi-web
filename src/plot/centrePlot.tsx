8be69f195bc3aa64108c55a9d2a2aa654fc45621
tizayi <tizayi0@gmail.com>
2023-09-05 16:45:29 +0100

    using h5web/lib

--- src/plot/centrePlot.tsx
+++ src/plot/centrePlot.tsx
@@ -1,13 +1,61 @@
 import { Box, Card, CardContent } from "@mui/material";
-import BeamlinePlot from "./dynamicPlot";
+import {
+  DataToHtml,
+  DefaultInteractions,
+  ResetZoomButton,
+  SvgCircle,
+  SvgRect,
+  SvgLine,
+  SvgElement,
+  VisCanvas,
+} from "@h5web/lib"
+import { Vector3 } from "three";
+
 
 export default function CentrePlot(): JSX.Element {
   return (
     <Box>
       <Card>
         <CardContent>
-          <div style={{ display: "grid", height: "60vh", width: "50vw", border: "2px solid black" }}>
-            <BeamlinePlot />
+          <div style={{ display: "grid", height: "60vh", width: "50vw", border: "solid black" }}>
+            <VisCanvas
+              style={{ padding: "5px 5px 5px" }}
+              abscissaConfig={{ visDomain: [0, 10], showGrid: true }}
+              ordinateConfig={{ visDomain: [0, 10], showGrid: true }}
+            >
+              <DefaultInteractions />
+              <ResetZoomButton />
+              <DataToHtml
+                points={[
+                  new Vector3(2, 8),
+                  new Vector3(4, 6),
+                  new Vector3(3, 2),
+                  new Vector3(6, 4),
+                  new Vector3(6, 6),
+                  new Vector3(7, 7),
+                ]}
+              >
+                {(pt1: Vector3, pt2: Vector3, pt3: Vector3, pt4: Vector3, pt5: Vector3, pt6: Vector3) => (
+                  <SvgElement>
+                    <SvgRect coords={[pt1, pt2]} fill="" />
+                    <SvgLine
+                      coords={[pt3, pt4]}
+                      stroke="darkblue"
+                      strokeWidth={5}
+                      strokeLinecap="round"
+                    />
+                    <SvgRect
+                      coords={[pt5, pt6]}
+                      fill="none"
+                      stroke="teal"
+                      strokeWidth={10}
+                      strokePosition="outside"
+                    />
+                    <SvgCircle coords={[pt5, pt6]} fill="none" stroke="lightseagreen" />
+                  </SvgElement>
+                )}
+              </DataToHtml>
+            </VisCanvas>
           </div>
         </CardContent>
       </Card>
