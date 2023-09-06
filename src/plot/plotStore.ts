ee1f2747d368c6308fdfe8bf77d8837f0bee4148
tizayi <tizayi0@gmail.com>
2023-09-06 11:44:07 +0100

    thinking about plot state

---
+++ src/plot/plotStore.ts
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+
+export enum PlotAxes {
+    milimeter,
+    pixel,
+    reciprocal,
+}
+
+interface PlotItem {
+    inPlot: boolean;
+    colour: string;
+}
+
+export interface PlotConfig {
+    detector: PlotItem;
+    beamstop: PlotItem;
+    cameraTube: PlotItem;
+    clearnace: PlotItem;
+    qrange: boolean;
+    mask: boolean;
+    calibrantInPlot: boolean;
+    calibrant: string;
+    plotAxes: PlotAxes;
+    edit: (newConfig: Partial<PlotConfig>) => void;
+}
+
+export const usePlotStore = create<PlotConfig>((set) => ({
+    detector: { inPlot: true, colour: "red" },
+    beamstop: { inPlot: true, colour: "blue" },
+    cameraTube: { inPlot: true, colour: "green" },
+    clearnace: { inPlot: true, colour: "pink" },
+    qrange: true,
+    mask: false,
+    calibrantInPlot: false,
+    calibrant: "something",
+    plotAxes: PlotAxes.milimeter,
+    edit: (newConfig) => { set({ ...newConfig }) }
+}));
