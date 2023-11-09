0642a11419243781a6c66fb09be5ff0ab8735eb5
tizayi <tizayi0@gmail.com>
2023-11-09 12:07:48 +0000

    better circle intersection range

--- src/calculations/ray.ts
+++ src/calculations/ray.ts
@@ -34,41 +34,16 @@ export class Ray {
     return new NumericRange(tMin, tMax);
   }
 
-  private getConicIntersectionParameterRange(
-    coeffOfx2: number,
-    coeffOfxy: number,
-    coeffOfy2: number,
-    coeffOfx: number,
-    coeffOfy: number,
-    constant: number,
-  ) {
+  public getCircleIntersectionParameterRange(radius: number, centre: Vector2) {
+    const diff = this.initial_point.clone().add(centre.multiplyScalar(-1))
+    const a = this.direction.dot(this.direction);
+    const b = 2*diff.dot(this.direction);
+    const c = (diff.dot(diff) - Math.pow(radius,2))
+    const discriminant = Math.pow(b, 2) - 4 * a * c;
+    
+
     let t1: number;
     let t2: number;
-
-    const a =
-      coeffOfx2 * Math.pow(this.direction.x, 2) +
-      coeffOfxy * this.direction.x * this.direction.y +
-      coeffOfy2 * Math.pow(this.direction.y, 2);
-
-    const b =
-      2 * coeffOfx2 * this.direction.x * this.initial_point.x +
-      coeffOfxy *
-        (this.direction.x * this.initial_point.y +
-          this.direction.y * this.initial_point.x) +
-      2 * coeffOfy2 * this.direction.y * this.initial_point.y +
-      coeffOfx * this.direction.x +
-      coeffOfy * this.direction.y;
-
-    const c =
-      coeffOfx2 * Math.pow(this.initial_point.x, 2) +
-      coeffOfxy * this.initial_point.x * this.initial_point.y +
-      coeffOfy2 * Math.pow(this.initial_point.y, 2) +
-      coeffOfx * this.initial_point.x +
-      coeffOfy * this.initial_point.y +
-      constant;
-
-    const discriminant = Math.pow(b, 2) - 4 * a * c;
-
     if (discriminant < 0) return null;
     if (a == 0) {
       if (b == 0)
@@ -79,36 +54,7 @@ export class Ray {
       t1 = (0.5 * (-b - Math.sqrt(discriminant))) / a;
       t2 = (0.5 * (-b + Math.sqrt(discriminant))) / a;
     }
-
     return this.getParameterRange(t1, t2);
-  }
-
-  private getEllipseIntersectionParameterRange(
-    a: number,
-    b: number,
-    centre: Vector2,
-  ) {
-    const coeffOfx2 = 1 / Math.pow(a, 2);
-    const coeffOfy2 = 1 / Math.pow(b, 2);
-    const coeffOfx = (-2 * centre.x) / Math.pow(a, 2);
-    const coeffOfy = (-2 * centre.y) / Math.pow(b, 2);
-    const constant =
-      Math.pow(centre.x, 2) / Math.pow(a, 2) +
-      Math.pow(centre.y, 2) / Math.pow(b, 2) -
-      1;
-
-    return this.getConicIntersectionParameterRange(
-      coeffOfx2,
-      0,
-      coeffOfy2,
-      coeffOfx,
-      coeffOfy,
-      constant,
-    );
-  }
-
-  public getCircleIntersectionParameterRange(radius: number, centre: Vector2) {
-    return this.getEllipseIntersectionParameterRange(radius, radius, centre);
   }
 
   public getRectangleIntersectionParameterRange(
@@ -146,3 +92,5 @@ export class Ray {
     return this.getParameterRange(result!.min, result!.max);
   }
 }
+
+
