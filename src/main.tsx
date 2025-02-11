552beeeffd19b921e980cf83bddcced316785668
tizayi <tizayi0@gmail.com>
2025-02-11 12:14:45 +0000

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app.tsx";
 import "./index.css";
 import { ErrorBoundary } from "react-error-boundary";
-import AppErrorFallBack from "./utils/errorComponants.tsx";
+import AppErrorFallBack from "./utils/errorComponents.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
